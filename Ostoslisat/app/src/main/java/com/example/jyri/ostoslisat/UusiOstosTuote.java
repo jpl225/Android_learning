a4323d5055b724a1ee1fd2f7bb883203f58f4c23
Jyri Lindroos <jyri.lindroos@gmail.com>
2018-06-05 20:26:36 +0300

    uusia tiedostoja

---
+++ Ostoslisat/app/src/main/java/com/example/jyri/ostoslisat/UusiOstosTuote.java
@@ -0,0 +1,13 @@
+package com.example.jyri.ostoslisat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UusiOstosTuote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uusi_ostos_tuote);
+    }
+}
