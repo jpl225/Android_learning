53aad7f427668a121567d92294314a96e9d2bd4b
Jyri Lindroos <jyri.lindroos@gmail.com>
2018-06-05 20:49:17 +0300

    vaihe 2

--- Ostoslisat/app/src/main/java/com/example/jyri/ostoslisat/MainActivity.java
+++ Ostoslisat/app/src/main/java/com/example/jyri/ostoslisat/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.jyri.ostoslisat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // LUODAAN PAINIKE
+            Button btn;
+        // LUODAAN VIITTAUS PAINIKKEESEEN XML-TIEDOSTOSTA
+            btn = (Button)findViewById(R.id.painike);
+        // LUODAAN TAPAHTUMANKUUNTELIJA PAINIKKEELLE
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    // LUODAAN INTENT-OLIO
+                        Intent siirryLisaykseen = new Intent (MainActivity.this, UusiOstosTuote.class);
+                    // KÄYNNISTETÄN SIIRTYMINEN
+                        startActivity(siirryLisaykseen);
+                }
+            });
+
     }
 }
--- Ostoslisat/app/src/main/java/com/example/jyri/ostoslisat/UusiOstosTuote.java
+++ Ostoslisat/app/src/main/java/com/example/jyri/ostoslisat/UusiOstosTuote.java
@@ -2,6 +2,7 @@ package com.example.jyri.ostoslisat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class UusiOstosTuote extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class UusiOstosTuote extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uusi_ostos_tuote);
+
+        // LUODAAN VERKKONÄKYMÄOLIO
+            WebView verkko;
+
+        // LUODAAN VIITTAUS OLIOON XML-TIEDOSTOSTA
+            verkko = (WebView)findViewById(R.id.verkkonakyma);
+
+        // HTML-TIEDOSTON LATAAMINEN OLIOON
+            verkko.loadUrl("file:///android_asset/ohjeet.html");
     }
 }
